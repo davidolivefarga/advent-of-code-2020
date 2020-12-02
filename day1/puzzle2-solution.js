17d36b06d1904f770dc1054fbc17462a5a1fcd5f
David Olivé <dolive@newrelic.com>
2020-12-02 07:51:50 +0100

    Day 1 - Fix typos

--- day1/puzzle2-solution.js
+++ day1/puzzle2-solution.js
@@ -1,14 +1,14 @@
 /*
 The objective is to find three different numbers of the input array
 that sum 2020 and multiply them. To do this, we can reuse the same
-approach as in puzzle 1. "e can go through the  array and for each
+approach as in puzzle 1. We can go through the array and for each
 element x we will do the following process:
 
 - Check if there are two numbers different than x that sum 2020 - x
   (notice this is the same calculation as in puzzle 1). If we do,
   we're finished and we just need to multiply them.
 
-Total complexity: O(n^2) time / O(n^2) memory.
+Total complexity: O(n^2) time / O(n) memory.
 */
 
 const nums = require("./input");
